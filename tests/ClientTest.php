55e56a79142447700c47523cdd14e18fc352c67f
Constantin Kraft <ckraft@smail.uni-koeln>
2016-05-19 17:39:08 +0200

    PHPUnit setup and first test for Client

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -7,18 +7,25 @@
 
 namespace Flowfact\Test;
 
+use Flowfact\Client;
+use Flowfact\Resources\UserType;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
 
     public function setUp()
     {
-        
+        $this->client = new Client('test', '123456', '301001',
+            'https://flowfactapi.flowfact.com/com.flowfact.server/api/rest/v1.0/customers', 'xml');
+
     }
     
     public function testGetUsers()
     {
-        // returns array of UserType
-
+        // should return array of UserType
+        $users = $this->client->getUsers();
+        foreach ($users as $user) {
+            $this->assertInstanceOf($user, UserType::class);
+        }
     }
 }
---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * User: connzen
+ * Date: 19.05.16
+ * Time: 17:07
+ */
+
+error_reporting(E_ALL | E_STRICT);
+require dirname(__DIR__) . '/vendor/autoload.php';
