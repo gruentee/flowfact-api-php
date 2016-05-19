a3a21bb08122317eecca67a0884b05270f72ead5
Constantin Kraft <ckraft@smail.uni-koeln>
2016-05-19 16:22:12 +0200

    Refactor: use constructor parameters instead of config file

--- src/Flowfact/Client.php
+++ src/Flowfact/Client.php
@@ -17,41 +17,42 @@ namespace Flowfact {
 
     class Client
     {
-        private $apiUser = null;
-        private $apiPass = null;
-        private $apiCustomerId = null;
-        private $apiUrl = null;
+        private $username = null;
+        private $password = null;
+        private $customerId = null;
+        private $baseUrl = null;
         private $acceptFormat = null;
         private $client = null;
         private $headers = array();
         private $requestOptions = array();
 
-        public function __construct($config='test', $acceptFormat='json')
+        public function __construct($username, $password, $customerId, $baseUrl, $acceptFormat = 'json')
         {
             // TODO: load configuration
-            $conf = require_once 'config/config.inc.php';
-
             // TODO: do config validation
-            $conf = $conf[$config];
-            $this->apiUser = $conf['username'];
-            $this->apiPass = $conf['password'];
-            $this->apiCustomerId = $conf['customer_id'];
-            $this->apiUrl = $conf['api_base_url'] . $this->apiCustomerId . "/";
+            $this->username = $customerId ."/" . $username;
+            $this->password = $password;
+            $this->customerId = $customerId;
+            $this->baseUrl = $baseUrl . '/' . $this->customerId . '/';
             $this->acceptFormat = $acceptFormat;
-            $this->requestOptions['auth'] = ["{$this->apiCustomerId}/{$this->apiUser}", $this->apiPass];
+            $this->requestOptions['auth'] = [$this->username, $this->password];
+            $this->requestOptions['headers'] = array(
+                'Accept' => 'application/' . $acceptFormat
+            );
             // configure Guzzle
             $this->client = new Http([
-                'base_uri' => $this->apiUrl
+                'base_uri' => $this->baseUrl
             ]);
         }
 
         /**
          * Get a list of users
-         * @return Users array
+         * @return UserType[]
          */
         public function getUsers()
         {
-            $url = 'users';
+            $url = strtolower(substr(__FUNCTION__, 3));
+//            $url = 'users';
             try {
                 $response = $this->client->get($url, $this->requestOptions);
                 // TODO: transform to array of User objects
---
+++ tests/ClientTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User: connzen
+ * Date: 19.05.16
+ * Time: 14:46
+ */
+
+namespace Flowfact\Test;
+
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        
+    }
+    
+    public function testGetUsers()
+    {
+        // returns array of UserType
+
+    }
+}
